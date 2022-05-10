7ef9a4e1a7b7c8869cc721e235ac8f30e0fa3dd2
Dev-Gopi <gopinath@gmail.com>
2022-05-10 14:37:12 +0530

    Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HelloWorld {
+    private JPanel Main;
+    private JTextArea display;
+    private JButton clickHereButton;
+    private JButton clearButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("HelloWorld");
+        frame.setContentPane(new HelloWorld().Main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public HelloWorld() {
+        clickHereButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                display.append(String.valueOf("Welcome To My Project\n"));
+                display.append(String.valueOf("Thank you for Visiting us"));
+            }
+        });
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                display.setText("");
+            }
+        });
+    }
+}
